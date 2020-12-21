83e1940015bf217d377a2c1f0d093e4eb886fe8f
Tudor Strainu <strainu.tudor@gmail.com>
2020-12-21 23:58:10 +0200

    add handle request guard

--- src/core/guards/user.guard.ts
+++ src/core/guards/user.guard.ts
@@ -2,5 +2,8 @@ import { IUserJwt } from '../models';
 import { ActivatedGuard } from './auth';
 
 export class AuthUserGuard extends ActivatedGuard {
- 
+  public handleRequest(err: any, user: IUserJwt): any {
+    super.handleRequest(err, user);
+    return user;
+  }
 }
