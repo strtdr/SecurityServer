cd6f2e645c5ca644fe2d6801f06cf8d73d44d129
Tudor Strainu <strainu.tudor@gmail.com>
2020-12-21 23:57:13 +0200

    core changes

--- src/core/guards/user.guard.ts
+++ src/core/guards/user.guard.ts
@@ -2,8 +2,5 @@ import { IUserJwt } from '../models';
 import { ActivatedGuard } from './auth';
 
 export class AuthUserGuard extends ActivatedGuard {
-  public handleRequest(err: any, user: IUserJwt): any {
-    super.handleRequest(err, user);
-    return user;
-  }
+ 
 }
